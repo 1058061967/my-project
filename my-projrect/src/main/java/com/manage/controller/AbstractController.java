b1956075904232ef802fb04c673a34b38da48e14
xiawenqiang <xiawenqiang@shbj.com>
2018-06-11 15:56:54 +0800

    [xwq] 提交

---
+++ my-projrect/src/main/java/com/manage/controller/AbstractController.java
@@ -0,0 +1,26 @@
+package com.manage.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.manage.entity.SysUserEntity;
+import com.manage.utils.ShiroUtils;
+
+/**
+ * Controller公共组件
+ * 
+ * @author chenshun
+ * @email sunlightcs@gmail.com
+ * @date 2016年11月9日 下午9:42:26
+ */
+public abstract class AbstractController {
+	protected Logger logger = LoggerFactory.getLogger(getClass());
+	
+	protected SysUserEntity getUser() {
+		return ShiroUtils.getUserEntity();
+	}
+
+	protected Long getUserId() {
+		return getUser().getUserId();
+	}
+}
